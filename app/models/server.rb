7fac3d1f2c7c1f70de2d0bbf8135b30e8874b534
Carlo D'Ugo <carlo.dugo@sitechhs.com>
2024-11-19 22:31:24 -0500

    added read status for servers

--- app/models/server.rb
+++ app/models/server.rb
@@ -1,5 +1,22 @@
 class Server < ApplicationRecord
-  has_many :server_members
+  has_many :server_members, dependent: :destroy
   has_many :users, through: :server_members
-  has_many :messages
+  has_many :messages, dependent: :destroy
+  has_many :server_read_states, dependent: :destroy
+
+  after_commit :create_read_state_for_owner, on: :create
+
+  def create_read_state_for_user(user)
+    server_read_states.create!(
+      user: user,
+      last_read_at: Time.current,
+      unread_count: 0
+    )
+  end
+
+  private
+
+  def create_read_state_for_owner
+    create_read_state_for_user(users.first) # owner is always the first user
+  end
 end
